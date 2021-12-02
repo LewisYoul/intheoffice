dc64995b16a3033eecf93ef88e3f0713197664dd
Lewis Youl <lewis.youl@trustflight.io>
2021-12-02 08:40:38 +0000

    chore: remove accounts

--- db/migrate/20210724165436_create_accounts.rb
+++
@@ -1,9 +0,0 @@
-class CreateAccounts < ActiveRecord::Migration[6.1]
-  def change
-    create_table :accounts do |t|
-      t.references :user, null: false
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_24_165436) do
+ActiveRecord::Schema.define(version: 2021_07_24_164757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
-
-  create_table "accounts", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_accounts_on_user_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
