3d0e1731ed054889761fc9cb42a64d1ec643fbed
Lewis Youl <lewis.youl@trustflight.io>
2021-12-10 08:52:08 +0000

    feat: add user location dropdown

--- app/controllers/authenticated/home_controller.rb
+++ app/controllers/authenticated/home_controller.rb
@@ -1,6 +1,9 @@
 module Authenticated
   class HomeController < AuthenticatedController
     def index
+      @today = Date.today
+      @current_week = @today.beginning_of_week..@today.end_of_week
+      @user_accounts = current_account.user_accounts.includes(:user)
     end
   end
 end
