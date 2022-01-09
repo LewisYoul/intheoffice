43fcd464242960537709c5869ad747f6ebd6252f
Lewis Youl <lewis.youl@trustflight.io>
2022-01-09 14:09:37 +0000

    feat: add my week to dashboard

--- app/controllers/authenticated/home_controller.rb
+++ app/controllers/authenticated/home_controller.rb
@@ -1,7 +1,23 @@
 module Authenticated
   class HomeController < AuthenticatedController
     def index
+      @week = date_range
+      @home, @office, @onlocation = Location.where(name: %w[office home onlocation]).order(name: :asc)
       @workplaces = current_account.workplaces.includes(:user_accounts)
+    end
+
+    private
+
+    def start_date
+      @start_date ||= Date.today.beginning_of_week
+    end
+
+    def end_date
+      @end_date ||= start_date.end_of_week
+    end
+
+    def date_range
+      start_date..end_date
     end
   end
 end
